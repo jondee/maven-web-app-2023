570c88878ad84c89f11d94a7c1f5e9fdde6bcf10
Tolu O <jondee@users.noreply.github.com>
2023-01-14 03:14:20 -0500

    Update EmployeeService.java

--- src/main/java/com/mt/services/EmployeeService.java
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -16,17 +16,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class EmployeeService {
 
 	
-	@RequestMapping(value = "/getEmployeeDetails", method = RequestMethod.GET)
+	@RequestMapping(value = "/getEmployeeDetails_API", method = RequestMethod.GET)
 	@ResponseBody
 	String uploadImage(HttpServletRequest request, HttpServletResponse response, HttpSession httpSession)
 			throws JSONException {
 
 		JSONObject js = new JSONObject();
-		js.put("Name", "Mithun Technologies");
-		js.put("Calling Name", "Mithun");
-		js.put("DOB", "08-Nov-2011");
-		js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
-		js.put("Places he like", "His native place");
+		js.put("Name", "Samsung Technologies");
+		js.put("Calling Name", "Samsung");
+		js.put("DOB", "08-Nov-2020");
+		js.put("Hobbies", "Reading Technical Blogs,Teaching, Playstation..");
+		js.put("Places he like", "New York City");
 
 		return js.toString();
 }
